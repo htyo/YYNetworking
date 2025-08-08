ff7e2ac9cc8f9efe582dbf9f616c3c228ca810c3
云庭 <yunting@viontv.local>
2025-08-08 15:32:31 +0800

    version:1.0.0,build:10101

--- YYNetworking/Classes/YYNetworking.h
+++ YYNetworking/Classes/YYNetworking.h
@@ -6,10 +6,57 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AFNetworking/AFNetworking.h>
+
+typedef NS_ENUM(NSUInteger, HTTPMethod) {
+    HTTPMethodGet       = 1,
+    HTTPMethodHead      = 2,
+    HTTPMethodPost      = 3,
+    HTTPMethodPut       = 4,
+    HTTPMethodDelete    = 5
+};
+
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface YYNetworking : NSObject
++ (AFHTTPSessionManager *)sessionManager;
+
++ (void)requestWithHTTPMethod:(HTTPMethod)method
+                    URLString:(nonnull NSString *)URLString
+                   parameters:(nullable NSDictionary *)parameters
+                      success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                      failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure;
+
++ (void)requestWithHTTPMethod:(HTTPMethod)method
+                    URLString:(nonnull NSString *)URLString
+                authorization:(nullable NSString *)authorization
+                   parameters:(nullable NSDictionary *)parameters
+                      success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                      failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure;
+
++ (void)requestWithHTTPMethod:(HTTPMethod)method
+                    URLString:(nonnull NSString *)URLString
+                      headers:(nullable NSDictionary *)headers
+                   parameters:(nullable NSDictionary *)parameters
+                      success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                      failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure;
+
++ (void)postRequestWithURLString:(nonnull NSString *)URLString
+                      parameters:(nullable NSDictionary *)parameters
+       constructingBodyBlock:(nullable void (^)(id <AFMultipartFormData> formData))block
+                        progress:(nullable void (^)(NSProgress * _Nonnull))uploadProgress
+                         success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                         failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure;
+
+
++ (void)postRequestWithURLString:(nonnull NSString *)URLString
+                   authorization:(nullable NSString *)authorization
+                      parameters:(nullable NSDictionary *)parameters
+       constructingBodyBlock:(nullable void (^)(id <AFMultipartFormData> formData))block
+                        progress:(nullable void (^)(NSProgress * _Nonnull))uploadProgress
+                         success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                         failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure;
 
 @end
 
--- YYNetworking/Classes/YYNetworking.m
+++ YYNetworking/Classes/YYNetworking.m
@@ -9,4 +9,158 @@
 
 @implementation YYNetworking
 
++ (AFHTTPSessionManager *)sessionManager {
+    static AFHTTPSessionManager * sessionManager;
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        sessionManager = [AFHTTPSessionManager manager];
+        sessionManager.requestSerializer = [AFJSONRequestSerializer serializer];
+        sessionManager.responseSerializer = [AFHTTPResponseSerializer serializer];
+        sessionManager.requestSerializer.HTTPShouldHandleCookies = YES;
+        sessionManager.requestSerializer.timeoutInterval = 10;
+        [sessionManager.requestSerializer setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+        sessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json",@"text/javascript",@"text/html",@"text/plain", nil];
+        
+        AFSecurityPolicy *securityPolicy = [AFSecurityPolicy defaultPolicy];
+        securityPolicy.validatesDomainName = NO;
+        securityPolicy.allowInvalidCertificates = YES;
+        sessionManager.securityPolicy = securityPolicy;
+        
+    });
+    return sessionManager;
+}
+
++ (void)requestWithHTTPMethod:(HTTPMethod)method
+                    URLString:(nonnull NSString *)URLString
+                   parameters:(nullable NSDictionary *)parameters
+                      success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                      failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure{
+    
+    [self requestWithHTTPMethod:method
+                      URLString:URLString
+                  authorization:nil
+                     parameters:parameters
+                        success:success
+                        failure:failure];
+}
++ (void)requestWithHTTPMethod:(HTTPMethod)method
+                    URLString:(nonnull NSString *)URLString
+                authorization:(nullable NSString *)authorization
+                   parameters:(nullable NSDictionary *)parameters
+                      success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                      failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure {
+    [self requestWithHTTPMethod:method
+                      URLString:URLString
+                        headers:@{@"Authorization":authorization}
+                     parameters:parameters
+                        success:success
+                        failure:failure];
+}
+
+
++ (void)requestWithHTTPMethod:(HTTPMethod)method
+                    URLString:(nonnull NSString *)URLString
+                      headers:(nullable NSDictionary *)headers
+                   parameters:(nullable NSDictionary *)parameters
+                      success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                      failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure {
+    
+    AFHTTPSessionManager *manager = [YYNetworking sessionManager];
+    
+    for(NSString * key in headers.allKeys) {
+        NSString * value = headers[key];
+        if (value && value.length) [manager.requestSerializer setValue:value forHTTPHeaderField:key];
+    }
+    
+    switch (method) {
+        case HTTPMethodGet: {
+            [manager GET:URLString
+              parameters:parameters
+                 headers:nil
+                progress:nil
+                 success:success
+                 failure:failure];
+            break;
+        }
+            
+        case HTTPMethodHead: {
+            [manager HEAD:URLString
+               parameters:parameters
+                  headers:nil
+                  success:^(NSURLSessionDataTask *_Nonnull task) { if (success) success(task,nil);}
+                  failure:failure];
+            break;
+        }
+            
+        case HTTPMethodPost: {
+            [manager POST:URLString
+               parameters:parameters
+                  headers:nil
+                 progress:nil
+                  success:success
+                  failure:failure];
+            break;
+        }
+            
+        case HTTPMethodPut: {
+            [manager PUT:URLString
+              parameters:parameters
+                 headers:nil
+                 success:success
+                 failure:failure];
+            break;
+        }
+            
+        case HTTPMethodDelete: {
+            [manager DELETE:URLString
+                 parameters:parameters
+                    headers:nil
+                    success:success
+                    failure:failure];
+            break;
+        }
+            
+        default:
+            break;
+    }
+}
+
+
++ (void)postRequestWithURLString:(nonnull NSString *)URLString
+                      parameters:(nullable NSDictionary *)parameters
+           constructingBodyBlock:(nullable void (^)(id <AFMultipartFormData> formData))block
+                        progress:(nullable void (^)(NSProgress * _Nonnull))uploadProgress
+                         success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                         failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure {
+    [self postRequestWithURLString:URLString
+                     authorization:nil
+                        parameters:parameters
+             constructingBodyBlock:block
+                          progress:uploadProgress
+                           success:success
+                           failure:failure];
+}
+
+
++ (void)postRequestWithURLString:(nonnull NSString *)URLString
+                   authorization:(nullable NSString *)authorization
+                      parameters:(nullable NSDictionary *)parameters
+           constructingBodyBlock:(nullable void (^)(id <AFMultipartFormData> formData))block
+                        progress:(nullable void (^)(NSProgress * _Nonnull))uploadProgress
+                         success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+                         failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure{
+    
+    AFHTTPSessionManager *manager = [YYNetworking sessionManager];
+    
+    if (authorization && authorization.length)[manager.requestSerializer setValue:authorization forHTTPHeaderField:@"Authorization"];
+    
+    [manager POST:URLString
+       parameters:parameters
+          headers:nil
+constructingBodyWithBlock:block
+         progress:uploadProgress
+          success:success
+          failure:failure];
+}
+
 @end
